d9abd2af0dfb3fdc9191e7b89a80cc9558b22422
dtxmendt <nguyendondong152@gmail.com>
2022-03-21 13:46:23 +0700

    i18n v2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -56,7 +56,7 @@ class PostsController < ApplicationController
     @post.destroy
 
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.html { redirect_to posts_url, notice: t("Post was successfully destroyed.") }
       format.json { head :no_content }
       format.js   { render layout: false }
     end
